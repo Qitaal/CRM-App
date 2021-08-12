9a518cc14ceef877aafe02a183fe98f958c71e5a
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-12 18:48:10 +0700

    Create model for leads app (Agent, Lead, User)

--- src/leads/models.py
+++ src/leads/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+# This is the Django build in user class
+# Django prefer to build our own user (custom user)
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+
+# Abstract class that implement a proper auth feature
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    # SOURCE_CHOICES = (
+    #     # first value is the value that get stored into database
+    #     # second value is the value that will displayed
+    #     ('YouTube', 'YouTube'),
+    #     ('Google', 'Google'),
+    #     ('Newsletter', 'Newsletter'),
+    # )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    # phone = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # We don't need to specified firstname & lastname 
+    # becous it already in AbstractUser class
+    # first_name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.email
