da5bb2338963fc8ec43888da90ace4dbe7a53f67
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-16 13:10:09 +0700

    create agent invitations

--- src/agents/forms.py
+++ src/agents/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.db import models
 from django.forms import fields
 from leads.models import Agent
 
+user = get_user_model()
+
 class AgentForm(forms.ModelForm):
     class Meta:
-        model = Agent
+        model = user
         fields = (
-            'user',
+            'email',
+            'username',
+            'first_name',
+            'last_name'
         )
