67b76cc0b182200222a31c584750b4641e132d42
Thalib Abu Qitaal <thalib74@gmail.com>
2021-08-19 14:02:25 +0700

    category

--- src/leads/urls.py
+++ src/leads/urls.py
@@ -6,6 +6,10 @@ from .views import (
     LeadCreateView,
     LeadUpdateView,
     LeadDeleteView,
+    AssignAgentView,
+    CategoryListView,
+    CategoryDetailView,
+    LeadCategoryUpdateView,
 )
 
 app_name = 'leads'
@@ -13,7 +17,11 @@ app_name = 'leads'
 urlpatterns = [
     path('', LeadListView.as_view(), name='lead_list'),
     path('<int:pk>/', LeadDetailView.as_view(), name='lead_detail'),
-    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead_update'),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign_agent'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead_delete'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead_update'),
     path('create/', LeadCreateView.as_view(), name='lead_create'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('<int:pk>/category/', LeadCategoryUpdateView.as_view(), name='lead_category_update'),
 ]
